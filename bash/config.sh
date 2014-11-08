44ac6fb0218c7b26555292651333fd8071ca8fd9
Andriel Nuernberg <andrielfn@gmail.com>
2014-11-08 19:25:10 -0200

    Update config file

--- bash/config.sh
+++ bash/config.sh
@@ -1,27 +1,31 @@
-## Editor ##
+# Editor
 export EDITOR="subl -w"
 
-## Git ##
+# Git
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM=auto
 
-## History ##
+# History
 export HISTCONTROL="ignoreboth:erasedups" # Erase duplicates in history
 export HISTSIZE=10000 # Store 10k history entries
 shopt -s histappend # Append to the history file when exiting instead of overwriting it
 
-## Customize the terminal input line
+# Customize the terminal input line
 prompt() {
-  PS1="$BLUE\w $YELLOW(\$(rb-version))$GREEN\$(__git_ps1) $RED$ $NORMAL"
+  PS1="${CYAN}\w ${MAGENTA}(\$(rb-version))${GREEN}\$(__git_ps1)\n${RED}$ ${NORMAL}"
 }
 
 PROMPT_COMMAND=prompt
 
-# Autojump
-[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
+# Rbenv
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-## PATH ##
+# PATH
 export GOPATH="$HOME/code/go"
 export PATH="$HOME/.bin:$PATH:$GOPATH/bin"
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# Autojump
+[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
