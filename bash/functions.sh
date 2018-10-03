cd73935e947f5ca580a2dcea5163eb1dad5f5853
Igor Florian <florian.igor@gmail.com>
2018-10-03 17:26:18 -0300

    Remove unused features

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,23 +1,8 @@
-# Get the process on a given port
-function port() {
-  lsof -i ":${1:-80}"
-}
-
-# Start an HTTP server from a directory, optionally specifying the port
-function server() {
-    local port="${1:-8000}"
-    python -m SimpleHTTPServer "$port"
-}
-
 # Create a directory and cd to it
 function mkcd {
   mkdir -p "$1" && cd "$1"
 }
 
-# Get the current branch
-function parse_git_branch {
-   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
 
 # Get the ruby version name using rbenv
 function rb-version {
